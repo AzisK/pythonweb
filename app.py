0d38b5615acd59a1d3241dac746cc2f30f14395f
AzisK <azuolas.krusna@yahoo.com>
2017-12-05 11:28:29 +0200

    Add meta tags to html. Add js files to run for the python server.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+import os
+
+JS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates\js')
+
+app = Flask(__name__, static_folder=JS)
 
 @app.route("/")
-def main():
-    return "Welcome!"
-
 def main():
     return render_template('index.html')
 
