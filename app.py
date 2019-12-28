1bb56db017a9834a73dd731d68745809b6f632c3
AzisK <azuolas.krusna@yahoo.com>
2019-12-28 17:58:17 +0200

    Remove unused code

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, flash, render_template, request, redirect
-from configparser import ConfigParser
+from flask import Flask, flash, render_template, redirect
 from flask_cors import CORS
-# import psycopg2
-import os
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SubmitField, DateField
 from wtforms.validators import DataRequired
@@ -12,12 +9,12 @@ from flask_migrate import Migrate
 from datetime import date
 
 # JS = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates\js")
-
 # app = Flask(__name__, static_folder=JS)
 
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -42,35 +39,16 @@ class InvoiceForm(FlaskForm):
     pvm = FloatField('pvm', validators=[DataRequired()])
     submit = SubmitField('Pridėk')
 
-    def __repr__(self):
-        return '<Invoice nr: {}>'.format(self.invoice)
-
-# def config(filename="database.ini", section="postgresql"):
-#     # create a parser
-#     parser = ConfigParser()
-#     # read config file
-#     parser.read(filename)
- 
-#     # get section, default to postgresql
-#     db = {}
-#     if parser.has_section(section):
-#         params = parser.items(section)
-#         for param in params:
-#             db[param[0]] = param[1]
-#     else:
-#         raise Exception("Section {0} not found in the {1} file".format(section, filename))
- 
-#     return db
-
-# params = config()
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
+
 @app.route("/invoices/")
 def invoices():
     return render_template("invoices.html")
+
 
 @app.route("/invoices/add/", methods=['POST'])
 def invoice_add():
@@ -94,28 +72,5 @@ def invoice_add():
     return redirect("/")
 
 
-# @app.route("/adddeal", methods = ["POST"])
-# def adddeal():
-#     try:
-#         deal = request.json
-#         conn = psycopg2.connect(**params)
-#         cur = conn.cursor()
-#         q = ("INSERT into deals (dates, invoice, company, amount, pvm) "
-#             "SELECT '{0}', '{1}', '{2}', '{3}', '{4}' "
-#             "WHERE NOT EXISTS (SELECT dates, invoice, company, amount, pvm FROM deals "
-#             "WHERE dates='{0}' AND invoice='{1}' AND company='{2}' AND amount='{3}' AND pvm='{4}');")
-#         sql = q.format(deal['date'], deal['invoice'], deal['company'], deal['sum'], deal['pvm'])
-#         print(sql)
-#         cur.execute(sql)
-#         conn.commit()
-#         print(deal, "saved to database!")
-#     except (Exception, psycopg2.DatabaseError) as error: 
-#         print(error)
-#     finally:
-#         if conn is not None:
-#             conn.close()
-#     return ",,!,,"
-
 if __name__ == "__main__":
     app.run()
-
